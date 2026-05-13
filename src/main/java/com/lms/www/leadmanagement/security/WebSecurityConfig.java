ecc60fbbf2c7bd083dde70aeae6b344d8f848cd2
Kona Ajay <konaajay0022@gmail.com>
2026-05-13 18:11:21 +0530

    ip

--- src/main/java/com/lms/www/leadmanagement/security/WebSecurityConfig.java
+++ src/main/java/com/lms/www/leadmanagement/security/WebSecurityConfig.java
@@ -67,7 +67,7 @@ public class WebSecurityConfig {
                         .requestMatchers("/api/auth/login", "/api/auth/forgot-password", "/api/auth/reset-password",
                                 "/error")
                         .permitAll()
-                        .requestMatchers("/api/public/**", "/api/payments/webhook/**", "/api/payments/fetch-order/**", "/api/payments/session/**").permitAll()
+                        .requestMatchers("/api/public/**", "/api/payments/**").permitAll()
 
                         // Restrict payment endpoints properly
                         .requestMatchers(HttpMethod.POST, "/api/payments/create").authenticated()
